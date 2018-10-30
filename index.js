f0e869f9df687d188b2fb6a365008f1017c89a55
github username <github email address>
2018-10-30 01:16:03 -0400

    Calcualated Total and Average salary of drivers

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ const driversByRevenue = (drivers) => {
 const driversByName = drivers => {
   return [...drivers].sort((x, y) => x.name.localeCompare(y.name));
 }
+//uses the reduce() method to sum the revenue of every driver and return the total
+const totalRevenue = (drivers) => {
+  return [...drivers].reduce((pay, driver) => pay + driver.revenue, 0)
+}
+//calculates the average revenue across all drivers: Total divided by number of drivers.
+ const averageRevenue = (drivers) => totalRevenue(drivers) / drivers.length
