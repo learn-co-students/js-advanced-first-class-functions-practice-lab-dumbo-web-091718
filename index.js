093ede85430caf4b09ce4311e921b7150e2df5c4
github username <github email address>
2018-10-30 00:55:06 -0400

    logged drivers hometown

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Code your solution in this file!
+//log by name
 const logDriverNames = function(drivers){
-  return drivers.forEach((driver) => 
+  return drivers.forEach((driver) =>
     console.log(driver.name));
+}
+//log by hometwon
+const logDriversByHometown = function(drivers, hometown){
+  return drivers.forEach( (driver) => {
+  if (driver.hometown == hometown)
+    {
+    console.log(driver.name)
+     }
+  })
 }
