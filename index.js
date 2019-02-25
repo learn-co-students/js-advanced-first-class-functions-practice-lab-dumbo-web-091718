aa18f3ce6b8cf533b5c9504de50c8b6a56526c58
Brian DiRito <bdirito91@gmail.com>
2019-02-24 22:56:45 -0500

    redid the lab for review

--- index.js
+++ index.js
@@ -1,49 +1,37 @@
 // Code your solution in this file!
-function logDriverNames(array) {
-  const callback = function(element) {
-    console.log(`${element.name}`)
-  }
-  array.forEach(callback)
-}
 
-function logDriversByHometown(array, hometown){
-  const callback = function(element) {
-    if (element.hometown == hometown)
-    console.log(`${element.name}`)
-  }
-  array.forEach(callback)
-}
-
-function driversByRevenue(array){
-  let newArray = array.slice()
-  const sorted = newArray.sort(function(a, b) {
-    return a.revenue - b.revenue
+function logDriverNames(drivers){
+  drivers.forEach(driver => {
+    console.log(driver.name)
   })
-  return sorted
 }
 
-function driversByName(array){
-  let newArray = array.slice()
-  const comparator = function(a, b) {
-    return a.name.localeCompare(b.name)
-  };
-  return newArray.sort(comparator)
+function logDriversByHometown(drivers, location){
+  drivers.forEach(driver => {
+    if (driver.hometown === location){
+      console.log(driver.name)
+    }
+  })
 }
 
-function totalRevenue(array){
-  let totalRev = 0;
-  array.forEach(function(element) {
-    totalRev += element.revenue
-  });
-  return totalRev
+function driversByRevenue(drivers){
+  const newArr = drivers.slice().sort((a, b) => { return a.revenue - b.revenue })
+  return newArr
 }
 
-function averageRevenue(array){
-  let averageRev;
-  let totalRev = 0;
-  array.forEach(function(element) {
-    totalRev += element.revenue
-  });
-  averageRev = (totalRev / array.length)
-  return averageRev
+function driversByName(drivers){
+  const newArr = drivers.slice().sort((a, b) => { return a.name.localeCompare(b.name) })
+  return newArr
+}
+
+function totalRevenue(drivers){
+  let revenue = 0
+  drivers.forEach(driver => {
+    revenue += driver.revenue
+  })
+  return revenue
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers) / drivers.length
 }
